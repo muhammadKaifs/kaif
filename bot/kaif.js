f7a4183fc5b3db8e7aa7208d7b219d64fdfcd09c
muhammadKaifs <155892524+muhammadKaifs@users.noreply.github.com>
2024-01-07 18:11:22 +0500

    added index.html

---
+++ bot/kaif.js
@@ -0,0 +1,23 @@
+<script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></script>
+<script>
+  window.botpressWebChat.init({
+      "composerPlaceholder": "Chat with Kaif",
+      "botConversationDescription": "Ask Any Question",
+      "botId": "2c742607-2361-4b73-a489-3a541ae33cb4",
+      "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
+      "messagingUrl": "https://messaging.botpress.cloud",
+      "clientId": "2c742607-2361-4b73-a489-3a541ae33cb4",
+      "webhookId": "b32c39ff-0768-47f7-81c0-4615f4b7cc24",
+      "lazySocket": true,
+      "themeName": "prism",
+      "botName": "Kaif",
+      "avatarUrl": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjEDB3NBEzb54m6HL5GpqAqogqU63UeKc_fA00gBxB8bopKPw1QGQQL1nhWW9ICvec1Qf9ZoIJklEsE7EPuHX9yADRpN-cgw6yHhphyK6z2DE9vN41wt-J-4Ioq2VceUW5uwr9ExX0ustVtipG1VLEAOWMrNvnVaTuvdR3I3X9Bi6s3lqZb4ji-PlYxLbc/s320/OIG.jpeg",
+      "privacyPolicy": "https://kaiffff.blogspot.com/2023/12/Kaifs.html",
+      "stylesheet": "https://webchat-styler-css.botpress.app/prod/0e120b93-a22f-45c0-ae98-7e6905a2cb2e/v92807/style.css",
+      "frontendVersion": "v1",
+      "useSessionStorage": true,
+      "enableConversationDeletion": true,
+      "theme": "prism",
+      "themeColor": "#2563eb"
+  });
+</script>
